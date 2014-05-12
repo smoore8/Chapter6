d949982f7772aceb3987de2c7614bcb76231fde6
smoore8 <savionmoore8@gmail.com>
2014-05-12 10:14:12 -0400

    Fixed

--- Exponets/Program.cs
+++ Exponets/Program.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Exponets
+namespace Exponent
 {
     class Program
     {
+        public static int Square(int a)
+        {
+            a = a * a;
+
+            return (a);
+        }
+        public static int Cube(int a)
+        {
+            a = a * a * a;
+
+            return (a);
+        }
+
         static void Main(string[] args)
         {
+            String input = Console.ReadLine();
+
+            int n = Convert.ToInt32(input);
+
+            Console.WriteLine(Square(n));
+            Console.WriteLine(Cube(n));
         }
     }
 }
