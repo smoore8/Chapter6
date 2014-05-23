fa40500842d055283aef46b191911f64a2b9cb27
smoore8 <savionmoore8@gmail.com>
2014-05-23 09:46:04 -0400

    Monogram done : )

--- Monogram/Program.cs
+++ Monogram/Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Monogram
+namespace Monogram.cs
 {
     class Program
     {
+        static void Monogram(char a, char b, char c)
+        {
+            Console.WriteLine("** {0}.{1}.{2}. **", a, b, c);
+        }
+
         static void Main(string[] args)
         {
+            char f1 = 'B', m1 = 'C', l1 = 'R', f2 = 'M', m2 = 'M', l2 = 'O';
 
+            Monogram(f1, m1, l1);
+            Monogram(f2, m2, l2);
         }
     }
 }
